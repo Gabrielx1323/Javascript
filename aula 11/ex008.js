9d2ef7cefeda3c4db315aba2b71205829cdd9b6f
Gabrielx1323 <gabrielfagundes09gf@gmail.com>
2025-05-28 11:37:56 -0300

    update

---
+++ aula 11/ex008.js
@@ -0,0 +1,7 @@
+var vel = 999
+if (vel > 80) {
+    console.log('Voce foi multado')
+} else {
+    console.log('Voce esta dentro da lei')
+}
+
---
+++ aula 11/ex009.js
@@ -0,0 +1,6 @@
+var pais = 'EUA'
+if (pais == 'Brasil') {
+    console.log('Voce e Brasileiro!')
+} else {
+    console.log('Voce e estrangeiro!')
+}

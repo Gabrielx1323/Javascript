82dda024a08a38e8ffd45f4f43c04bc3b9a8742b
Gabrielx1323 <gabrielfagundes09gf@gmail.com>
2025-06-04 11:32:37 -0300

    update

---
+++ aula12ex/ex015/script.js
@@ -0,0 +1,6 @@
+function vrf() {
+var idade = document.getElementById('Idate')
+var idade = Number(idadeInput.value)
+var msg = document.getElementById('msg')
+msg.innerText = `sua idade ${idade}`
+}

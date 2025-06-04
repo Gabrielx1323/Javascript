6f4a7750f4609ae58a0f7beed7f4e9410a579bd1
Gabrielx1323 <gabrielfagundes09gf@gmail.com>
2025-06-04 10:42:46 -0300

    update

--- aula12ex/ex14/script.js
+++ aula12ex/ex14/script.js
@@ -2,18 +2,19 @@ function carregar() {
 var msg = window.document.getElementById('msg')
 var img = window.document.getElementById('img')
 var data = new Date()
-var hora = data.getHours()
+//var hora = data.getHours()
 var min = new Date()
-var mi = min.getMinutes()
-var hora = 5
-msg.innerHTML = `Agora são ${hora}:${minutos < 10 ? '0' + minutos : minutos}`;
+//var mi = min.getMinutes()
+var hora = 4
+var mi = 10
+msg.innerHTML = `Agora são ${hora}:${mi}`;
 if (hora >= 0 && hora <= 5) {
-    img.src = 'Noite-removebg-preview.png'
+    img.src = 'noite.png'
 } else if (hora >= 6 && hora <= 12) {
-    img.src = 'manha-removebg-preview.png'
+    img.src = 'manha.png'
 } else if (hora >= 13 && hora <= 18) {
-    img.src = 'tarde-removebg-preview.png'
+    img.src = 'tarde.png'
 } else if (hora >= 19 && hora <= 23) {
-    img.src = 'Noite-removebg-preview.png'
+    img.src = 'noite.png'
 }
 }

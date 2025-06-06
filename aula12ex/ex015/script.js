d4454c58ea14740fac4677465ee9df8c4b8d1ff4
Gabrielx1323 <gabrielfagundes09gf@gmail.com>
2025-06-06 11:24:57 -0300

    update

--- aula12ex/ex015/script.js
+++ aula12ex/ex015/script.js
@@ -1,6 +1,18 @@
 function vrf() {
-var idade = document.getElementById('Idate')
-var idade = Number(idadeInput.value)
-var msg = document.getElementById('msg')
-msg.innerText = `sua idade ${idade}`
+var data = new Date()
+var ano = data.getFullYear()
+var fano = document.getElementById('txtano')
+var res = document.getElementById('res')
+if (fano.value.legth == 0 || Number(fano.value) > ano) {
+    window.alert('[ERRO] Verifique os dados e tente novamente!')
+}  else {
+    var fsex = document.getElementsByClassName('radsex')
+    var idade = ano - Number(fano.value)
+    var genero = ''
+    var img = document.createElement('img')
+    img.setAttribute('id', 'foto')
+    if (idade >= 0 && idade < 10) {
+        img.setAttribute('src', 'baby-male.jpg')
+    } else if (2)
+}
 }

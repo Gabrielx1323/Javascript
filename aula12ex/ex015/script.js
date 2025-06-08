0518df42f2ee9c2a5e574d6afb4f6745cddbbcac
Gabrielx1323 <gabrielfagundes09gf@gmail.com>
2025-06-08 15:01:31 -0300

    update

--- aula12ex/ex015/script.js
+++ aula12ex/ex015/script.js
@@ -11,8 +11,32 @@ if (fano.value.legth == 0 || Number(fano.value) > ano) {
     var genero = ''
     var img = document.createElement('img')
     img.setAttribute('id', 'foto')
+    if (fsex[0].checked) {
+        genero = 'Gosta De Femea'
     if (idade >= 0 && idade < 10) {
         img.setAttribute('src', 'baby-male.jpg')
-    } else if (2)
+    } else if (idade < 21) {
+        img.setAttribute('src', 'young-male.jpg')
+    } else if (idade < 50) {
+        img.setAttribute('src', 'adult-male.jpg')
+    } else  {
+       img.setAttribute('src', 'elder-male.jpg') 
+    }
+} else if (fsex[1].checked) {
+    genero = 'Gosta De Macho'
+    if (idade >= 0 && idade < 10) {
+    img.setAttribute('src', 'baby-male.jpg')  
+    } else if (idade < 21) {
+      img.setAttribute('src', 'young-male.jpg')  
+    } else if (idade < 50) {
+       img.setAttribute('src', 'adult-male.jpg')
+    } else {
+       img.setAttribute('src', 'elder-male.jpg')
+    }
 }
+res.style.textAlign = 'center'
+res.innerHTML = `Detectamos ${genero} com ${idade} anos`
+res.appendChild(img)
+}
+
 }

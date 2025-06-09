493379c8628c65417af28a8cd037abe444ca38eb
Gabrielx1323 <gabrielfagundes09gf@gmail.com>
2025-06-09 15:50:39 -0300

    update

---
+++ aula 13/ex13.js
@@ -0,0 +1,27 @@
+var c = 1
+for (c=1;c<=10;c++) {
+    console.log(`passo ${c}`)
+}
+console.log(`fim`)
+
+
+
+
+
+
+
+
+/*
+var c = 1
+do {
+    console.log(`Pedaço ${c}`)
+    c++ 
+} while (c <= 6)
+*/
+/*
+var c = 1
+while (c <= 10) {
+    console.log(`Pedaço ${c}`)
+    c++
+} 
+*/
